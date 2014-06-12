3efcb93b566e664e3cb30a7228059dc631940617
Naomi Pentrel <t-naopen@microsoft.com>
2014-06-12 18:16:16 +0100

    adding search after 5 sec

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 var app = angular.module("GitHubViewer", []);
 
-var MainController = function($scope, $http) {
+var MainController = function($scope, $http, $interval, $log) {
 
 	var onUserComplete = function(response) {
 		$scope.user = response.data;
@@ -17,8 +17,23 @@ var MainController = function($scope, $http) {
 		$scope.repos = response.data;
 	}
 
+	var decrementCountdown = function(){
+		$scope.countdown -= 1;
+		if($scope.countdown < 1){
+			$scope.search($scope.username);
+		}
+	};
+
+	var countdownInterval = null;
+
+	var startCountdown = function() {
+		countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+	};
+
 	$scope.search = function(username) {
+		$log.info("Searching for " + username);
 		$http.get("https://api.github.com/users/" + username).then(onUserComplete, onError);
+		$scope.countdown = 0;
 	};
 
 //	$http.get("https://api.github.com/users/angular").then(onUserComplete, onError);
@@ -26,9 +41,11 @@ var MainController = function($scope, $http) {
 	$scope.username = "angular";
 	$scope.message = "Github Viewer";
 	$scope.repoSortOrder = "-stargazers_count";
+	$scope.countdown = 5;
+	startCountdown();
 
 };
 
-app.controller("MainController", ["$scope", "$http", MainController]);
+app.controller("MainController", ["$scope", "$http", "$interval", "$log", MainController]);
 
 }());
